826d677b224b5e8f99834862d77162ac1b16691b
Lisnake1412 <lisnake2002@gmail.com>
2023-11-29 13:53:58 +0700

    update v3

--- src/pages/Chart/index.js
+++ src/pages/Chart/index.js
@@ -1,101 +1,83 @@
 import { useState } from 'react';
-import styles from './Chart.module.scss';
+import html2canvas from 'html2canvas';
 import classNames from 'classnames/bind';
+import styles from './Chart.module.scss';
+
+
 const cx = classNames.bind(styles);
 
-const CHART_DATAs = [
-    {
-        name: 'BTC',
-        value: 60.47,
-        isLong: 0,
-    },
-    {
-        name: 'BTC',
-        value: 177.67,
-        isLong: 1,
-    },
-    {
-        name: 'BTC',
-        value: -30,
-        isLong: 1,
-    },
-    {
-        name: 'ERP',
-        value: -148.77,
-        isLong: 0,
-    },
-    {
-        name: 'ERP',
-        value: -30.77,
-        isLong: 0,
-    },
-    
-];
-
-
-
-
-
-
-
 function Chart() {
-    const [dataList, setDataList] = useState([])
-    const [max_column_value, setColumnLenght] = useState(300)
+    const [dataList, setDataList] = useState([]);
+    const [max_column_value, setColumnLenght] = useState(300);
     function addMoreColumn() {
-        let coinName = document.getElementById("coinName").value
-        let coinValue = document.getElementById("coinValue").value
-        let isLong = document.getElementById("long").checked
-        setDataList(prev => {
-            return (
-                [
-                    ...prev,
-                    {
-                        name:coinName,
-                        value:coinValue,
-                        isLong:isLong
-                    }
-                    
-                ]
-            )
-        })
+        let coinName = document.getElementById('coinName').value;
+        let coinValue = document.getElementById('coinValue').value;
+        let isLong = document.getElementById('long').checked;
+        setDataList((prev) => {
+            return [
+                ...prev,
+                {
+                    name: coinName,
+                    value: coinValue,
+                    isLong: isLong,
+                },
+            ];
+        });
     }
     let getColumnArray = () => {
         let index = 0;
         let returnArray = [];
         while (index * 50 <= max_column_value) {
             returnArray[index] = index * 50;
-            index++
+            index++;
         }
         return returnArray;
     };
-    let column_array = getColumnArray()
+    let column_array = getColumnArray();
     function deleteLastColumn() {
-        setDataList(prev => prev.slice(0, -1))
+        setDataList((prev) => prev.slice(0, -1));
     }
-    const currentDate = new Date()
+
+    function capture() {
+        html2canvas(document.querySelector("#capture")).then(canvas => {
+            
+            document.body.appendChild(canvas)
+        });
+        
+    }
+
+    const currentDate = new Date();
     return (
         <>
             <div className={cx('input-wrapper')}>
-                <input id='coinName' placeholder='tên coin'/>
+                <input id="coinName" placeholder="tên coin" />
                 <br></br>
-                <input id='coinValue' placeholder='giá trị: (%)'/>
+                <input id="coinValue" placeholder="giá trị: (%)" />
                 <br></br>
-                <p>long</p> <input id="long"  name='long' value={1} type='radio' />
-                <p>short</p> <input  name="long" value={0} type='radio'/>
+                <p>long</p> <input id="long" name="long" value={1} type="radio" />
+                <p>short</p> <input name="long" value={0} type="radio" />
                 <br></br>
                 <button onClick={() => addMoreColumn()}>Thêm</button>
                 <button onClick={() => deleteLastColumn()}>Xóa</button>
-                <br/>
+                <button onClick={() => capture()}>Chụp ảnh</button>
+                <br />
                 <p>_______________________</p>
-                <br/>
+                <br />
                 <p>Điều chỉnh độ cao cột</p>
-                <button onClick={() => setColumnLenght(prev => prev - 50)}>{'<'}</button>
-                <button onClick={() => setColumnLenght(prev => prev + 50)}>{'>'}</button>
+                <button onClick={() => setColumnLenght((prev) => prev - 50)}>{'<'}</button>
+                <button onClick={() => setColumnLenght((prev) => prev + 50)}>{'>'}</button>
             </div>
-            <div className={cx('wrapper')}>
+            <h3 style={{margin:10, color:"red"}}>demo:</h3>
+            <div className={cx('wrapper')} id='capture'> 
                 <h2>tổng kết lợi nhuận</h2>
                 <h3>{`ngày ${currentDate.getDate()}/${currentDate.getMonth()}/${currentDate.getFullYear()} `}</h3>
                 <div className={cx('content')}>
+                    <div className={cx('long-short')}>
+                        <div className={cx('long')}></div>
+                        <span>Long</span>
+                        <div className={cx('short')}></div>
+                        <span>Short</span>
+                    </div>
                     <div className={cx('percent-column')}>
                         {column_array.map((data, index) => {
                             return <p>{`${data}%`}</p>;
@@ -103,14 +85,14 @@ function Chart() {
                     </div>
                     {dataList.map((data, index) => {
                         let height_value = '0';
-                        let height = 0
+                        let height = 0;
                         let isPositive = 0;
                         if (data.value >= 0) {
                             height_value = `${(data.value / max_column_value) * 100}%`;
-                            height = (data.value / max_column_value) * 100
+                            height = (data.value / max_column_value) * 100;
                         } else {
                             height_value = `${(-data.value / max_column_value) * 100}%`;
-                            height = (-data.value / max_column_value) * 100
+                            height = (-data.value / max_column_value) * 100;
                             isPositive = 1;
                         }
                         let isRed = !data.isLong;
@@ -120,9 +102,11 @@ function Chart() {
                                 <div className={cx('circle')}>
                                     <div></div>
                                 </div>
-                                {height > 10 && <div className={cx('triangle')}>
-                                    <div></div>
-                                </div>}
+                                {height > 10 && (
+                                    <div className={cx('triangle')}>
+                                        <div></div>
+                                    </div>
+                                )}
                                 <div className={cx('left-part')}></div>
                                 <div className={cx('right-part')}></div>
                                 <div className={cx('bottom-content')}>
@@ -134,6 +118,7 @@ function Chart() {
                     })}
                 </div>
             </div>
+            <h3 style={{margin:10, color:"red"}}>Ảnh sau khi xuất(chuột phải để lưu):</h3>
         </>
     );
 }
